56babfd3b68ffae84ed7918aec338a7cf4e92fcd
prajjwal-singh-db <166522222+prajjwal-singh-db@users.noreply.github.com>
2024-04-10 16:01:34 +0530

    Update Grep.java

--- courses/data_analysis/lab2/javahelp/src/main/java/com/google/cloud/training/dataanalyst/javahelp/Grep.java
+++ courses/data_analysis/lab2/javahelp/src/main/java/com/google/cloud/training/dataanalyst/javahelp/Grep.java
@@ -1,58 +1,97 @@
 /*
- * Copyright (C) 2016 Google Inc.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-package com.google.cloud.training.dataanalyst.javahelp;
 
+import java.util.Arrays;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
+
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.Count;
+import org.apache.beam.sdk.transforms.Filter;
+import org.apache.beam.sdk.transforms.FlatMapElements;
+import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.transforms.PTransform;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.TypeDescriptors;
+import org.apache.beam.sdk.transforms.Flatten;
+import org.apache.beam.sdk.transforms.Sample;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.ParDo;
 
-/**
- * A dataflow pipeline that prints the lines that match a specific search term
- * 
- * @author vlakshmanan
- *
- */
-public class Grep {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-	@SuppressWarnings("serial")
-	public static void main(String[] args) {
-		PipelineOptions options = PipelineOptionsFactory.fromArgs(args).withValidation().create();
-		Pipeline p = Pipeline.create(options);
 
-		String input = "src/main/java/com/google/cloud/training/dataanalyst/javahelp/*.java";
-		String outputPrefix = "/tmp/output";
-		final String searchTerm = "import";
+public class Task {
 
-		p //
-				.apply("GetJava", TextIO.read().from(input)) //
-				.apply("Grep", ParDo.of(new DoFn<String, String>() {
-					@ProcessElement
-					public void processElement(ProcessContext c) throws Exception {
-						String line = c.element();
-						if (line.contains(searchTerm)) {
-							c.output(line);
-						}
-					}
-				})) //
-				.apply(TextIO.write().to(outputPrefix).withSuffix(".txt").withoutSharding());
+    private static final Logger LOG = LoggerFactory.getLogger(Task.class);
 
-		p.run();
-	}
+
+    public static void main(String[] args) {
+
+        PipelineOptions options = PipelineOptionsFactory.create();
+
+        // Create the Pipeline object with the options we defined above
+        Pipeline pipeline = Pipeline.create(options);
+        PCollection<String> input = pipeline.apply(TextIO.read().from("gs://apache-beam-samples/nyc_taxi/misc/sample1000.csv"));
+
+        PCollection<Double> rideTolalAmounts = input.apply(ParDo.of(new ExtractTaxiRideCostFn()));
+        final PTransform<PCollection<Double>, PCollection<Iterable<Double>>> sample = Sample.fixedSizeGlobally(10);
+        PCollection<Double> rideAmounts = rideTolalAmounts.apply(sample)
+                .apply(Flatten.iterables())
+                .apply("Log amounts", ParDo.of(new LogDouble()));
+
+        pipeline.run().waitUntilFinish();
+
+    }
+
+    private static Double tryParseTaxiRideCost(String[] inputItems) {
+        try {
+            return Double.parseDouble(tryParseString(inputItems, 16));
+        } catch (NumberFormatException | NullPointerException e) {
+            return 0.0;
+        }
+    }
+
+
+    private static String tryParseString(String[] inputItems, int index) {
+        return inputItems.length > index ? inputItems[index] : null;
+    }
+
+    static class ExtractTaxiRideCostFn extends DoFn<String, Double> {
+
+        @ProcessElement
+        public void processElement(ProcessContext c) {
+            String[] items = c.element().split(",");
+            Double totalAmount = tryParseTaxiRideCost(items);
+            c.output(totalAmount);
+        }
+    }
+
+    public static class LogDouble extends DoFn<Double, Double> {
+
+        @ProcessElement
+        public void processElement(ProcessContext c) throws Exception {
+            LOG.info("Total Amount: {}", c.element());
+            c.output(c.element());
+        }
+    }
 }
